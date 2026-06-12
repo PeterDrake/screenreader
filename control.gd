04a3f04a0128efe950f5dbddf096f0133158d968
Peter Drake <drake@lclark.edu>
2026-06-12 09:29:55 -0700

    Buttons print when pushed; next element after button 1 is button 3
    ... but VoiceOver doesn't recognize this

--- control.gd
+++ control.gd
@@ -1,5 +1,21 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/Button.grab_focus.call_deferred()
+	$VBoxContainer/Button1.grab_focus.call_deferred()
 	
+
+
+func _on_button_1_button_down() -> void:
+	print("Button 1 pushed")
+	
+
+
+
+func _on_button_2_button_down() -> void:
+	print("Button 2 pushed")
+	
+
+
+
+func _on_button_3_button_down() -> void:
+	print("Button 3 pushed")
